8ff891fa251d93388fe0f2c0f1c94a11710e25a4
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-07-24 21:48:24 -0300

    Music Player 24-07-24
    1 atualizacao.

---
+++ Js - Music Player/music_player.js
@@ -0,0 +1,9 @@
+const num_musics = 3
+const PP = document.getElementById('Play_Pause')
+const MA = document.getElementById('music_audio')
+
+/*function play(){
+    MA.play()}
+
+PP.addEventListener('click', play) */
+a
